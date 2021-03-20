f6306169fc7f19cbb90dd2e426c2ff77de000a98
aardpro <239479@qq.com>
2021-03-20 13:43:48 +0800

    vue-image-slim

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueImageSlim from '@/vue-image-slim.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueImageSlim
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-image-slim />
+  </div>
+</template>
